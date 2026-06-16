198aea5e0b5c18cbb9fc359cdd94e650b5dc79a5
Chris VanDoo <cvandood@gmail.com>
2026-06-16 14:39:23 -0400

    add semi-colons and a few other fixes

--- util/build-with-cache.js
+++ util/build-with-cache.js
@@ -3,6 +3,7 @@ import fs  from 'node:fs';
 import path from 'node:path';
 import process from 'node:process'
 import crypto from 'node:crypto'
+import { execSync } from 'node:child_process';
 
 const DEFAULT_CACHE_DIR = ".cache"
 
@@ -18,25 +19,25 @@ function getImportsForFile(file) {
 
   let dependencies = new Map();
   for (let i = 0; i < imports.length; i++) {
-    let dep = imports[i].n
+    let dep = imports[i].n;
     if (path.extname(dep) == ".js") {
-      dep = path.join(path.dirname(file), dep)
+      dep = path.join(path.dirname(file), dep);
     }
 
     if (! dependencies.has(dep) && fs.existsSync(dep)) {
-      let res = getImportsForFile(dep)
-      res.forEach((value, key) => dependencies.set(key, value))
+      let res = getImportsForFile(dep);
+      res.forEach((value, key) => dependencies.set(key, value));
     } 
-    dependencies.set(dep, true)
+    dependencies.set(dep, true);
   }
 
-  return dependencies
+  return dependencies;
 }
 
 // Checks if dep matches anything in package.json
 function isThirdPartyDep(dep) {
-  const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
-  const allDeps = { ...pkg.dependencies, ...pkg.devDependencies }
+  const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+  const allDeps = { ...pkg.dependencies, ...pkg.devDependencies };
   return Object.keys(allDeps).some(name => dep === name || dep.startsWith(name + '/'));
 }
 
@@ -47,15 +48,15 @@ function computeHash(file) {
     return;
   }
 
-  const hash = crypto.createHash('sha256')
-  hash.update(`${process.version} ${process.platform} ${process.arch}`)
-  hash.update(fs.readFileSync(file))
+  const hash = crypto.createHash('sha256');
+  hash.update(`${process.version} ${process.platform} ${process.arch}`);
+  hash.update(fs.readFileSync(file));
 
   let dependencies = getImportsForFile(file);
   let hashPackageFiles = false;
   dependencies.forEach((_, key) => {
     if (fs.existsSync(key)) {
-      hash.update(fs.readFileSync(key))
+      hash.update(fs.readFileSync(key));
     } else {
       // this is probably a 3rd party or stdlib dep
       if ( isThirdPartyDep(key) && ! hashPackageFiles ) {
@@ -65,29 +66,33 @@ function computeHash(file) {
   })
 
   if (hashPackageFiles) {
-    hash.update(fs.readFileSync('package.json'))
-    hash.update(fs.readFileSync('package-lock.json'))
+    hash.update(fs.readFileSync('package.json'));
+    hash.update(fs.readFileSync('package-lock.json'));
   }
 
-  return hash.digest('hex')
+  return hash.digest('hex');
 }
 
+// Builds <input> with `node` and caches the <output>
 function BuildWithCache(input, output) {
-  cacheDir = getCacheDirName();
+  console.log(`Executing node ${input} to build ${output}...`);
+
+  let cacheDir = getCacheDirName();
   if (! fs.existsSync(cacheDir)) {
-    fs.mkdirSync(cacheDir, {recursive: true})
+    fs.mkdirSync(cacheDir, {recursive: true});
   }
 
-  hash = computeHash(input)
-  target = path.join(cacheDir, hash)
+  let hash = computeHash(input);
+  let target = path.join(cacheDir, hash);
   if (fs.existsSync(target)) {
-    console.log(`Found cached ${output} for ${input} hash: ${hash}. Skipping build and using cached file...`)
-    fs.copyFileSync(target, output)
+    console.log(`Found cached ${output} for ${input} hash: ${hash}. Skipping build and using cached file...`);
+    fs.copyFileSync(target, output);
   } else {
-    // run node to build input
-    fs.copyFileSync(output, target)
+    const res = execSync(`node ${input}`, {encoding: 'utf-8'});
+    console.log(res)
+    fs.copyFileSync(output, target);
   }
 }
 
 await init;
-console.log(computeHash('util/build.js'));
+BuildWithCache(process.argv[2], process.argv[3]);
